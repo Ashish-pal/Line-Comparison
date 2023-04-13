b77a1306ad1fb3954a0da4d16f6a0ff0c9b23195
Ashish Pal <ak987982@gmail.com>
2023-04-13 12:47:32 +0530

    Added Welcome Message

---
+++ src/line_comparison/LineComparisonMain.java
@@ -0,0 +1,7 @@
+package line_comparison;
+
+public class LineComparisonMain {
+    public static void main(String[] args) {
+        System.out.printf("Welcome to Line Comparison Computation Program");
+    }
+}
